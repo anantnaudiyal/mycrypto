fa5f41e79512999786aa6c68fb8b60cc60303dfa
Anant Naudiyal <94854426+anantnaudiyal@users.noreply.github.com>
2024-09-05 08:39:23 +0530

    Delete client directory

--- client/src/store/appStore.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import watchlistReducer from "./watchlistSlice";
-import userReducer from "./userSlice";
-export const appStore = configureStore({
-  reducer: {
-    watchlist: watchlistReducer,
-    user: userReducer, // Add your other reducers here.  e.g., user: userReducer,
-  },
-});
